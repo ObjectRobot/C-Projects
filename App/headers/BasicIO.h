367230ff4848a1df80bb9215d5507a2ae8136f56
Oliver Kokolee <ollie12245@gmail.com>
2022-10-06 10:47:48 -0400

    Decisions and BasicIO

---
+++ App/headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Oliver"
+#define LAST_NAME "Kokolee"
+#define WEIGHT 170.5
+#define AGE 21
+#include <stdio.h>
+
+// include function declarations
+void output(void);
+void input(void);
